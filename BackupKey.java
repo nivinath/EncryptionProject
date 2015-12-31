1fa28745b1ff2f0c27157834e345e03ea7acaf52
Elizabeth Cho <SilverSapo@users.noreply.github.com>
2015-12-30 23:19:30 -0500

    Update BackupKey.java

--- BackupKey.java
+++ BackupKey.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 public class BackupKey
 {
 	private String alphabet;
@@ -21,48 +19,5 @@ public class BackupKey
 			cipher = after + before;
 			return cipher;
 	}
-	
-	public String removeDuplicates()
-	{
-		HashMap<Character, Integer> mapping = new HashMap<Character, Integer>();
-		for (int i = 0; i < alphabet.length(); i++)
-		{
-			mapping.put(Character.valueOf(alphabet.charAt(i)), Integer.valueOf(0));
-		}
-		for (int i = alphabet.length() - 1; i >= 0; i--)
-		{
-			if (!mapping.containsKey(Character.valueOf(alphabet.charAt(i))))
-			{
-				return "Contains non-letter characters";
-			}
-			else
-			{
-				int intConversion = mapping.get(Character.valueOf(alphabet.charAt(i))).intValue();
-				intConversion ++;
-				Integer newValue = Integer.valueOf(intConversion);
-				mapping.put(Character.valueOf(alphabet.charAt(i)), newValue);
-			}
-		}
-		for (int i = alphabet.length() - 1; i >= 0; i--)
-		{
-			if(mapping.get(Character.valueOf(alphabet.charAt(i))).intValue() > 1)
-			{
-				int intConversion = mapping.get(Character.valueOf(alphabet.charAt(i))).intValue();
-				intConversion--;
-				Integer newValue = Integer.valueOf(intConversion);
-				mapping.put(Character.valueOf(alphabet.charAt(i)), newValue);
-				alphabet = alphabet.substring(0, i) + alphabet.substring(i + 1);
-			}
-		}
-		
-		alphabet.toString();
-		return alphabet;
-		}
-		
-	}
-	
-	public String toString()
-	{
-		return this.alphabet;
-	}
+
 }
